114bb26cc7204ddaac48149f4aed5182d57c948a
Ricardo <ricolvs123@gmail.com>
2019-06-19 22:30:22 -0300

    WIP2

--- src/parser/component/parser_return_statement_matcher.cpp
+++ src/parser/component/parser_return_statement_matcher.cpp
@@ -3,9 +3,11 @@
 #include "symbols_keywords.h"
 #include "my_except.h"
 
-//ReturnStatement = keyword 'return' , Expression ;
+//ReturnStatement = keyword 'return' , Expression , ';' ;
 
 std::shared_ptr<Parser::ReturnStatement> Parser::ReturnStatementMatcher::makeMatch(parserProgress &p){
     if(!p.isKeyword(KEYWORD_RETURN))throw MyExcept::NoMatchException(p.get_nothrow_nonull()->line,"expected 'return', got '"+p.get_nothrow_nonull()->get_literal()+"'");
-    return std::make_shared<ReturnStatement>(ExpressionMatcher().makeMatch(p));
+    std::shared_ptr<Expression> expr=ExpressionMatcher().makeMatch(p);
+    if(!p.isSymbol(SYMBOL_SEMICOLON))throw MyExcept::NoMatchException(p.get_nothrow_nonull()->line,"expected ';', got '"+p.get_nothrow_nonull()->get_literal()+"'");
+    return std::make_shared<ReturnStatement>(expr);
 }
