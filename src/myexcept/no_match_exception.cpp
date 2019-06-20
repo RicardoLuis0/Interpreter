8d240134eb34c9d2b26769964b815271ea996e82
Ricardo <ricolvs123@gmail.com>
2019-06-19 21:47:10 -0300

    improve exception message

--- src/myexcept/no_match_exception.cpp
+++ src/myexcept/no_match_exception.cpp
@@ -1,3 +1,3 @@
 #include "no_match_exception.h"
 
-MyExcept::NoMatchException::NoMatchException(int line,const std::string& s):runtime_error("No Match for \""+s+"\" in line "+std::to_string(line)){}
+MyExcept::NoMatchException::NoMatchException(int line,const std::string& s):runtime_error(s+" in line "+std::to_string(line)){}
