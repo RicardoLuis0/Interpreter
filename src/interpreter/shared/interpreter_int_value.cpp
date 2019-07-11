e3986055daf76db44a4b149b54681a7e58f3acb5
RicardoLuis0 <ricolvs123@gmail.com>
2019-07-11 11:08:29 -0300

    int operators implementation

--- src/interpreter/shared/interpreter_int_value.cpp
+++ src/interpreter/shared/interpreter_int_value.cpp
@@ -1,5 +1,7 @@
 #include "interpreter_int_value.h"
 
+#include "interpreter_float_value.h"
+
 using namespace Interpreter;
 
 IntValue::IntValue(int i):value(i){}
@@ -15,81 +17,112 @@ std::shared_ptr<Parser::VarType> IntValue::get_type(){
     return std::make_shared<Parser::VarType>(Parser::PRIMITIVE_INT);
 }
 
-//TODO IntValue operators
+std::shared_ptr<Value> IntValue::lt(std::shared_ptr<Value> &other){
+    return std::make_shared<IntValue>(value<(typeid(*other)==typeid(IntValue)?std::dynamic_pointer_cast<IntValue>(other)->get():std::dynamic_pointer_cast<FloatValue>(other)->get()));
+}
 
-std::shared_ptr<Value> IntValue::lt(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '<'");
+std::shared_ptr<Value> IntValue::gt(std::shared_ptr<Value> &other){
+    return std::make_shared<IntValue>(value>(typeid(*other)==typeid(IntValue)?std::dynamic_pointer_cast<IntValue>(other)->get():std::dynamic_pointer_cast<FloatValue>(other)->get()));
 }
-std::shared_ptr<Value> IntValue::gt(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '>'");
+
+std::shared_ptr<Value> IntValue::eq(std::shared_ptr<Value> &other){
+    return std::make_shared<IntValue>(value==(typeid(*other)==typeid(IntValue)?std::dynamic_pointer_cast<IntValue>(other)->get():std::dynamic_pointer_cast<FloatValue>(other)->get()));
 }
-std::shared_ptr<Value> IntValue::eq(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '=='");
+
+std::shared_ptr<Value> IntValue::neq(std::shared_ptr<Value> &other){
+    return std::make_shared<IntValue>(value==(typeid(*other)!=typeid(IntValue)?std::dynamic_pointer_cast<IntValue>(other)->get():std::dynamic_pointer_cast<FloatValue>(other)->get()));
 }
-std::shared_ptr<Value> IntValue::neq(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '!='");
+
+std::shared_ptr<Value> IntValue::gt_eq(std::shared_ptr<Value> &other){
+    return std::make_shared<IntValue>(value>=(typeid(*other)==typeid(IntValue)?std::dynamic_pointer_cast<IntValue>(other)->get():std::dynamic_pointer_cast<FloatValue>(other)->get()));
 }
-std::shared_ptr<Value> IntValue::gt_eq(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '>='");
+
+std::shared_ptr<Value> IntValue::lt_eq(std::shared_ptr<Value> &other){
+    return std::make_shared<IntValue>(value<=(typeid(*other)==typeid(IntValue)?std::dynamic_pointer_cast<IntValue>(other)->get():std::dynamic_pointer_cast<FloatValue>(other)->get()));
 }
-std::shared_ptr<Value> IntValue::lt_eq(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '<='");
+
+std::shared_ptr<Value> IntValue::mod(std::shared_ptr<Value> &other){
+    return std::make_shared<IntValue>(value%std::dynamic_pointer_cast<IntValue>(other)->get());
 }
-std::shared_ptr<Value> IntValue::mod(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '%'");
+
+std::shared_ptr<Value> IntValue::bitwise_and(std::shared_ptr<Value> &other){
+    return std::make_shared<IntValue>(value&std::dynamic_pointer_cast<IntValue>(other)->get());
 }
- std::shared_ptr<Value> IntValue::bitwise_and(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '&'");
+
+std::shared_ptr<Value> IntValue::bitwise_or(std::shared_ptr<Value> &other){
+    return std::make_shared<IntValue>(value|std::dynamic_pointer_cast<IntValue>(other)->get());
 }
-std::shared_ptr<Value> IntValue::bitwise_or(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '|'");
+
+std::shared_ptr<Value> IntValue::bitwise_xor(std::shared_ptr<Value> &other){
+    return std::make_shared<IntValue>(value^std::dynamic_pointer_cast<IntValue>(other)->get());
 }
-std::shared_ptr<Value> IntValue::bitwise_xor(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '^'");
+
+std::shared_ptr<Value> IntValue::logical_and(std::shared_ptr<Value> &other){
+    return std::make_shared<IntValue>(value&&std::dynamic_pointer_cast<IntValue>(other)->get());
 }
-std::shared_ptr<Value> IntValue::logical_and(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '&&'");
+
+std::shared_ptr<Value> IntValue::logical_or(std::shared_ptr<Value> &other){
+    return std::make_shared<IntValue>(value||std::dynamic_pointer_cast<IntValue>(other)->get());
 }
-std::shared_ptr<Value> IntValue::logical_or(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '||'");
+
+std::shared_ptr<Value> IntValue::add(std::shared_ptr<Value> &other){
+    return std::make_shared<IntValue>(value+(typeid(*other)==typeid(IntValue)?std::dynamic_pointer_cast<IntValue>(other)->get():std::dynamic_pointer_cast<FloatValue>(other)->get()));
 }
-std::shared_ptr<Value> IntValue::add(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '+'");
+
+std::shared_ptr<Value> IntValue::sub(std::shared_ptr<Value> &other){
+    return std::make_shared<IntValue>(value-(typeid(*other)==typeid(IntValue)?std::dynamic_pointer_cast<IntValue>(other)->get():std::dynamic_pointer_cast<FloatValue>(other)->get()));
 }
-std::shared_ptr<Value> IntValue::sub(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '-'");
+
+std::shared_ptr<Value> IntValue::mul(std::shared_ptr<Value> &other){
+    return std::make_shared<IntValue>(value*(typeid(*other)==typeid(IntValue)?std::dynamic_pointer_cast<IntValue>(other)->get():std::dynamic_pointer_cast<FloatValue>(other)->get()));
 }
-std::shared_ptr<Value> IntValue::mul(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '*'");
+
+std::shared_ptr<Value> IntValue::div(std::shared_ptr<Value> &other){
+    return std::make_shared<IntValue>(value/(typeid(*other)==typeid(IntValue)?std::dynamic_pointer_cast<IntValue>(other)->get():std::dynamic_pointer_cast<FloatValue>(other)->get()));
 }
-std::shared_ptr<Value> IntValue::div(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '/'");
+
+std::shared_ptr<Value> IntValue::assign(std::shared_ptr<Value> &other){
+    value=(typeid(*other)==typeid(IntValue)?std::dynamic_pointer_cast<IntValue>(other)->get():std::dynamic_pointer_cast<FloatValue>(other)->get());
+    return nullptr;
 }
-std::shared_ptr<Value> IntValue::assign(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '='");
+
+std::shared_ptr<Value> IntValue::assign_add(std::shared_ptr<Value> &other){
+    value+=(typeid(*other)==typeid(IntValue)?std::dynamic_pointer_cast<IntValue>(other)->get():std::dynamic_pointer_cast<FloatValue>(other)->get());
+    return nullptr;
 }
-std::shared_ptr<Value> IntValue::assign_add(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '+='");
+
+std::shared_ptr<Value> IntValue::assign_sub(std::shared_ptr<Value> &other){
+    value-=(typeid(*other)==typeid(IntValue)?std::dynamic_pointer_cast<IntValue>(other)->get():std::dynamic_pointer_cast<FloatValue>(other)->get());
+    return nullptr;
 }
-std::shared_ptr<Value> IntValue::assign_sub(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '-='");
+
+std::shared_ptr<Value> IntValue::assign_mul(std::shared_ptr<Value> &other){
+    value*=(typeid(*other)==typeid(IntValue)?std::dynamic_pointer_cast<IntValue>(other)->get():std::dynamic_pointer_cast<FloatValue>(other)->get());
+    return nullptr;
 }
-std::shared_ptr<Value> IntValue::assign_mul(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '*='");
+
+std::shared_ptr<Value> IntValue::assign_div(std::shared_ptr<Value> &other){
+    value/=(typeid(*other)==typeid(IntValue)?std::dynamic_pointer_cast<IntValue>(other)->get():std::dynamic_pointer_cast<FloatValue>(other)->get());
+    return nullptr;
 }
-std::shared_ptr<Value> IntValue::assign_div(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '/='");
+
+std::shared_ptr<Value> IntValue::assign_mod(std::shared_ptr<Value> &other){
+    value%=std::dynamic_pointer_cast<IntValue>(other)->get();
+    return nullptr;
 }
-std::shared_ptr<Value> IntValue::assign_mod(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '%='");
+
+std::shared_ptr<Value> IntValue::assign_bitwise_and(std::shared_ptr<Value> &other){
+    value&=std::dynamic_pointer_cast<IntValue>(other)->get();
+    return nullptr;
 }
-std::shared_ptr<Value> IntValue::assign_bitwise_and(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '&='");
+
+std::shared_ptr<Value> IntValue::assign_bitwise_or(std::shared_ptr<Value> &other){
+    value|=std::dynamic_pointer_cast<IntValue>(other)->get();
+    return nullptr;
 }
-std::shared_ptr<Value> IntValue::assign_bitwise_or(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '|='");
-}
-std::shared_ptr<Value> IntValue::assign_bitwise_xor(std::shared_ptr<Value>&){
-    throw std::runtime_error("unimplemented operator '^='");
+
+std::shared_ptr<Value> IntValue::assign_bitwise_xor(std::shared_ptr<Value> &other){
+    value^=std::dynamic_pointer_cast<IntValue>(other)->get();
+    return nullptr;
 }
 
