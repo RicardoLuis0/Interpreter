726d05f43d078e87400fca8d1769419efc3807a5
Ricardo <ricolvs123@gmail.com>
2019-07-29 20:38:44 -0300

    putchar, ftos native functions

--- src/deflib.cpp
+++ src/deflib.cpp
@@ -4,17 +4,18 @@
 #include "parser_var_type.h"
 #include "interpreter_string_value.h"
 #include "interpreter_int_value.h"
+#include "interpreter_float_value.h"
 #include "conio.h"
 
 //NOTE native functions to make later
-//void printf(string format,mixed ... args)
+//void printf(string format, ... args)
 //void puts(string) DONE
-//void putc(int) 
+//void putchar(int) DONE
 //string getline() DONE
 //int stoi(string) DONE
 //float stof(string)
 //string itos(int) DONE
-//string ftos(float)
+//string ftos(float) DONE
 //void cls() DONE
 //int getch() DONE
 
@@ -100,6 +101,25 @@ namespace Interpreter {
         }
     };
 
+    class ftos : public Function{
+
+        std::string get_name() override {
+            return "ftos";
+        }
+
+        std::shared_ptr<Type> get_type(){
+            return Type::string_type();
+        }
+
+        std::vector<FunctionParameter> get_parameters() override {
+            return {{Type::float_type(),"float",false}};
+        }
+
+        std::shared_ptr<Value> call(ExecFrame * parent_frame,std::vector<std::shared_ptr<Value>> args) override {
+            return std::make_shared<StringValue>(std::to_string(std::dynamic_pointer_cast<FloatValue>(args[0])->get()));
+        }
+    };
+
     class cls : public Function{
 
         std::string get_name() override {
@@ -138,13 +158,36 @@ namespace Interpreter {
             return std::make_shared<IntValue>(::getch());
         }
     };
+
+    class putchar : public Function{
+
+        std::string get_name() override {
+            return "putchar";
+        }
+
+        std::shared_ptr<Type> get_type(){
+            return Type::void_type();
+        }
+
+        std::vector<FunctionParameter> get_parameters() override {
+            return {{Type::int_type(),"ch",false}};
+        }
+
+        std::shared_ptr<Value> call(ExecFrame * parent_frame,std::vector<std::shared_ptr<Value>> args) override {
+            ::putchar(std::dynamic_pointer_cast<IntValue>(args[0])->get());
+            return nullptr;
+        }
+    };
+
 }
 
 void Interpreter::init_deflib(DefaultFrame * d){
     d->register_function(std::make_shared<Interpreter::puts>());
+    d->register_function(std::make_shared<Interpreter::putchar>());
     d->register_function(std::make_shared<Interpreter::getline>());
     d->register_function(std::make_shared<Interpreter::stoi>());
     d->register_function(std::make_shared<Interpreter::itos>());
+    d->register_function(std::make_shared<Interpreter::ftos>());
     d->register_function(std::make_shared<Interpreter::cls>());
     d->register_function(std::make_shared<Interpreter::getch>());
 }
