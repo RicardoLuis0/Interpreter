4861c3facf7c249b0c1961522f74348b14b7f34e
RicardoLuis0 <ricolvs123@gmail.com>
2019-07-29 05:00:04 -0300

    misc ExecFrame improvements

--- src/interpreter/exec/interpreter_exec_frame.cpp
+++ src/interpreter/exec/interpreter_exec_frame.cpp
@@ -7,6 +7,8 @@
 #include "interpreter_string_variable.h"
 #include "interpreter_user_function.h"
 #include "interpreter_int_type.h"
+#include "interpreter_float_type.h"
+#include "interpreter_string_type.h"
 #include "interpreter_expression.h"
 
 using namespace Interpreter;
@@ -14,14 +16,11 @@ using namespace Interpreter;
 ExecFrame::ExecFrame(ExecFrame * p,DefaultFrame * d):parent(p),defaults(d){
     for(auto vpair:defaults->variable_defaults){
         if(typeid(*vpair.second)==typeid(IntVariable)){
-            std::shared_ptr<IntVariable> var(std::dynamic_pointer_cast<IntVariable>(vpair.second));
-            variables.insert({var->get_name(),std::make_shared<IntVariable>(var->get_name(),var->get())});
+            variables.insert({vpair.second->get_name(),std::make_shared<IntVariable>(vpair.second->get_name(),std::dynamic_pointer_cast<IntVariable>(vpair.second)->get())});
         }else if(typeid(*vpair.second)==typeid(FloatVariable)){
-            std::shared_ptr<FloatVariable> var(std::dynamic_pointer_cast<FloatVariable>(vpair.second));
-            variables.insert({var->get_name(),std::make_shared<FloatVariable>(var->get_name(),var->get())});
+            variables.insert({vpair.second->get_name(),std::make_shared<FloatVariable>(vpair.second->get_name(),std::dynamic_pointer_cast<FloatVariable>(vpair.second)->get())});
         }else if(typeid(*vpair.second)==typeid(StringVariable)){
-            std::shared_ptr<StringVariable> var(std::dynamic_pointer_cast<StringVariable>(vpair.second));
-            variables.insert({var->get_name(),std::make_shared<StringVariable>(var->get_name(),var->get())});
+            variables.insert({vpair.second->get_name(),std::make_shared<StringVariable>(vpair.second->get_name(),std::dynamic_pointer_cast<StringVariable>(vpair.second)->get())});
         }else{
             throw std::runtime_error("unknown variable type in defaults");
         }
@@ -40,33 +39,24 @@ std::shared_ptr<Function> ExecFrame::get_function(std::string name,std::vector<F
 }
 
 void ExecFrame::set_variable(std::string s,std::shared_ptr<Value> val,std::shared_ptr<Type> t){
-    if(CHECKPTR(val,IntValue)){
-        if(CHECKPTR(t,IntType)){
-            variables[s]=std::make_shared<IntVariable>(s,std::dynamic_pointer_cast<IntValue>(val)->get());
-        }else{
-            variables[s]=std::make_shared<FloatVariable>(s,std::dynamic_pointer_cast<IntValue>(val)->get());
-        }
-    }else if(CHECKPTR(val,FloatValue)){
-        if(CHECKPTR(t,IntType)){
-            variables[s]=std::make_shared<IntVariable>(s,std::dynamic_pointer_cast<FloatValue>(val)->get());
-        }else{
-            variables[s]=std::make_shared<FloatVariable>(s,std::dynamic_pointer_cast<FloatValue>(val)->get());
-        }
-    }else if(CHECKPTR(val,StringValue)){
-        variables[s]=std::make_shared<StringVariable>(s,std::dynamic_pointer_cast<StringValue>(val)->get());
+    if(CHECKPTR(t,IntType)){
+        variables[s]=std::make_shared<IntVariable>(s,std::dynamic_pointer_cast<IntValue>(val->get_type()->cast(val,t))->get());
+    }else if(CHECKPTR(t,FloatType)){
+        variables[s]=std::make_shared<FloatVariable>(s,std::dynamic_pointer_cast<FloatValue>(val->get_type()->cast(val,t))->get());
+    }else if(CHECKPTR(t,StringType)){
+        variables[s]=std::make_shared<StringVariable>(s,std::dynamic_pointer_cast<StringValue>(val->get_type()->cast(val,t))->get());
+    }else{
+        throw std::runtime_error("classes not implemented");
     }
 }
 
 void ExecFrame::set_args(std::map<std::string,std::pair<std::shared_ptr<Value>,std::pair<bool,std::shared_ptr<Type>>>> args){
    for(std::pair<std::string,std::pair<std::shared_ptr<Value>,std::pair<bool,std::shared_ptr<Type>>>> arg:args){
-        if(arg.second.second.first){
-            if(CHECKPTR(arg.second.first,Variable)){
-                variables[arg.first]=std::dynamic_pointer_cast<Variable>(arg.second.first);
-                continue;
-            }
-            //intentional fallthrough
+        if(arg.second.second.first&&CHECKPTR(arg.second.first,Variable)){
+            variables[arg.first]=std::dynamic_pointer_cast<Variable>(arg.second.first);
+        }else{
+            set_variable(arg.first,arg.second.first,arg.second.second.second);
         }
-        set_variable(arg.first,arg.second.first,arg.second.second.second);
     }
 }
 
