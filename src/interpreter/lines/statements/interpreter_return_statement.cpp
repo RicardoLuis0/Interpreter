f054601fefca38bab343668307911c36aac65804
RicardoLuis0 <ricolvs123@gmail.com>
2019-08-16 14:45:32 -0300

    ReturnStatement type checking

--- src/interpreter/lines/statements/interpreter_return_statement.cpp
+++ src/interpreter/lines/statements/interpreter_return_statement.cpp
@@ -1,11 +1,29 @@
 #include "interpreter_return_statement.h"
 #include "interpreter_line_result_return.h"
+#include "interpreter_user_function.h"
+#include "my_except.h"
 
 using namespace Interpreter;
 
 ReturnStatement::ReturnStatement(DefaultFrame * context,std::shared_ptr<Parser::ReturnStatement> stmt):Line(stmt->line_start),value(nullptr),line(stmt->line_start){
+    DefaultFrame * func_frame=context;
+    while(!(func_frame->is_function)){
+        if(func_frame->parent==nullptr){
+            throw MyExcept::SyntaxError(source_line,source_line,"return statement called outside of a function (HOW\?\?\?)");//shouldn't be possible, EVER
+        }
+        func_frame=func_frame->parent;
+    }
     if(stmt->value){
        value=std::make_shared<Expression>(context,stmt->value);
+    }
+    if(value!=nullptr&&func_frame->func->get_type()){
+        if(!value->get_type()->allows_implicit_cast(value->get_type(),func_frame->func->get_type())){
+            throw MyExcept::SyntaxError(source_line,source_line,"in function "+func_frame->func->get_name()+"(...), cannot "+((value->get_type()->has_cast(value->get_type(),func_frame->func->get_type()))?"implicitly ":"")+"cast "+value->get_type()->get_name()+" to "+func_frame->func->get_type()->get_name());
+        }
+    }else if(func_frame->func->get_type()){
+        throw MyExcept::SyntaxError(source_line,source_line,"in function "+func_frame->func->get_name()+"(...), return statement missing return value");
+    }else if(value){
+        throw MyExcept::SyntaxError(source_line,source_line,"in function "+func_frame->func->get_name()+"(...), void function cannot return a value");
     }
 }
 
