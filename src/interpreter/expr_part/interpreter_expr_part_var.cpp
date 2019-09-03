c1ca940e9637c019452f1588eb4e38377b53d16e
RicardoLuis0 <ricolvs123@gmail.com>
2019-09-03 17:20:41 -0300

    fix using invalid variable

--- src/interpreter/expr_part/interpreter_expr_part_var.cpp
+++ src/interpreter/expr_part/interpreter_expr_part_var.cpp
@@ -4,8 +4,11 @@
 
 using namespace Interpreter;
 
-ExprPartVar::ExprPartVar(DefaultFrame * context,std::string s,int line):type(context->get_variable_type(ident)),ident(s){
+ExprPartVar::ExprPartVar(DefaultFrame * context,std::string s,int line,bool ignore_const):type(context->get_variable_type(s)),ident(s){
     if(type==nullptr)throw MyExcept::SyntaxError(line,line,"undefined variable "+ident);
+    if(ignore_const){
+        type=type->change_const(type,false);
+    }
 }
 
 std::shared_ptr<Type> ExprPartVar::get_type(){
