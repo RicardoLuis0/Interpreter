67766e1d6426e09b4d0ffb9c56a73f0457945b93
Ricardo <ricolvs123@gmail.com>
2019-10-08 19:23:03 -0300

    misc

--- src/parser/component/parser_struct_definition_matcher.cpp
+++ src/parser/component/parser_struct_definition_matcher.cpp
@@ -1,10 +1,15 @@
 #include "parser_struct_definition_matcher.h"
+#include "symbols_keywords.h"
 
 using namespace Parser;
 
 //StructDefinition = keyword 'struct' , symbol '{' , { MemberDefinition } , symbol '}' ;
 
 std::shared_ptr<StructDefinition> StructDefinitionMatcher::makeMatch(parserProgress &p){
+    if(p.isKeyword(KEYWORD_STRUCT)){
+        
+    }else{
+        throw MyExcept::NoMatchException(-1,"not a struct");
+    }
     //TODO
-    throw std::runtime_error("unimplemented");
 }
