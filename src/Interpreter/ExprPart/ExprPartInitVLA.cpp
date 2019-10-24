b0d95bd4d1489b516951539f87a0bb2cac32a458
RicardoLuis0 <ricolvs123@gmail.com>
2019-10-24 13:03:51 -0300

    formatting

--- src/Interpreter/ExprPart/ExprPartInitVLA.cpp
+++ src/Interpreter/ExprPart/ExprPartInitVLA.cpp
@@ -5,9 +5,11 @@ using namespace Interpreter;
 ExprPartInitVLA::ExprPartInitVLA(std::shared_ptr<ArrayType> a,std::string n):arr_type(a),name(n){
     
 }
+
 std::shared_ptr<Type> ExprPartInitVLA::get_type(){
     return Type::void_type();
 }
+
 std::shared_ptr<Value> ExprPartInitVLA::eval(ExecFrame * context){
     auto type=arr_type->build_vla_array_type(context);
     context->variables[name]=type->make_variable(type,name);
