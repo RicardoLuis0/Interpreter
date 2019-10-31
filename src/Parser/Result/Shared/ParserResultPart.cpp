7919bd1ce969cf4acd340be2664494f46655966a
RicardoLuis0 <ricolvs123@gmail.com>
2019-10-31 01:54:37 -0300

    ...

--- src/Parser/Result/Shared/ParserResultPart.cpp
+++ src/Parser/Result/Shared/ParserResultPart.cpp
@@ -6,5 +6,5 @@ ParserResultPart::ParserResultPart(int ls,int le):line_start(ls),line_end(le){
 }
 
 std::string ParserResultPart::get_indent(int depth){
-    return depth>0?"|   "+get_indent(depth-1):"";
+    return depth>0?"| "+get_indent(depth-1):"";
 }
