37683c508e5678db248aa12033df0ce177e23616
RicardoLuis0 <ricolvs123@gmail.com>
2020-04-30 06:13:04 -0300

    fatal exception type

--- include/MyExcept/NoMatchException.h
+++ include/MyExcept/NoMatchException.h
@@ -5,11 +5,18 @@
 #include <stdexcept>
 #include "Parser/Parser.h"
 
+
 namespace MyExcept{
     class NoMatchException : public std::runtime_error {
         public:
             explicit NoMatchException(int line,const std::string& s);
             explicit NoMatchException(Parser::parserProgress &p,const std::string& expected);
+    };
+    class NoMatchExceptionFatal : public std::runtime_error {
+        public:
+            explicit NoMatchExceptionFatal(int line,const std::string& s);
+            explicit NoMatchExceptionFatal(Parser::parserProgress &p,const std::string& expected);
+            explicit NoMatchExceptionFatal(NoMatchException &e);
     };
 };
 
--- src/MyExcept/NoMatchException.cpp
+++ src/MyExcept/NoMatchException.cpp
@@ -2,8 +2,12 @@
 #include "Parser/Parser.h"
 
 
+
+
 MyExcept::NoMatchException::NoMatchException(int line,const std::string& s):runtime_error(s+" in line "+std::to_string(line)){}
 MyExcept::NoMatchException::NoMatchException(Parser::parserProgress &p,const std::string& x):NoMatchException(p.get_nothrow_nonull()->line,"expected "+x+", got '"+p.get_nothrow_nonull()->get_literal()+"'"){}
 
-
+MyExcept::NoMatchExceptionFatal::NoMatchExceptionFatal(int line,const std::string& s):runtime_error(s+" in line "+std::to_string(line)){}
+MyExcept::NoMatchExceptionFatal::NoMatchExceptionFatal(Parser::parserProgress &p,const std::string& x):NoMatchExceptionFatal(p.get_nothrow_nonull()->line,"expected "+x+", got '"+p.get_nothrow_nonull()->get_literal()+"'"){}
+MyExcept::NoMatchExceptionFatal::NoMatchExceptionFatal(NoMatchException &e):runtime_error(e.what()){}
 
