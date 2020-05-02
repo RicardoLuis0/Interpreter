6661fdbe5fe081d4051b1659b0515bb82500baa5
RicardoLuis0 <ricolvs123@gmail.com>
2020-05-02 01:59:39 -0300

    fix old code

--- src/Parser/Component/Definition/VariableDefinitionItemMatcher.cpp
+++ src/Parser/Component/Definition/VariableDefinitionItemMatcher.cpp
@@ -12,7 +12,7 @@ using namespace Parser;
 std::shared_ptr<VariableDefinitionItem> VariableDefinitionItemMatcher::makeMatch(parserProgress &p){
     int line_start=p.get_line();
     std::shared_ptr<Lexer::Token> t;
-    if(!(t=p.isType(Lexer::TOKEN_TYPE_WORD)))MyExcept::NoMatchException(p.get_nothrow_nonull()->line,"expected identifier, got '"+p.get_nothrow_nonull()->get_literal()+"'");
+    if(!(t=p.isType(Lexer::TOKEN_TYPE_WORD)))throw MyExcept::NoMatchException(p.get_nothrow_nonull()->line,"expected identifier, got '"+p.get_nothrow_nonull()->get_literal()+"'");
     std::shared_ptr<Expression> e=nullptr;
     if(p.isSymbol(SYMBOL_ASSIGNMENT)){
         e=ExpressionMatcher().makeMatch(p);
