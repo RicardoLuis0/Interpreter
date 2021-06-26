2484d51734cb520d41ebd762f9ab5404e6553aac
Ricardo Luís Vaz Silva <ricolvs123@gmail.com>
2021-06-26 14:05:23 -0300

    helper shell script

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+if command -v RBuild &> /dev/null; then
+    RBuild="RBuild"
+else
+    RBuild="./.RBuild"
+
+    mkdir -p .deps
+    need_run=true
+    if [ -d ".deps/RBuild" ]; then
+        echo "Updating RBuild"
+        old_head=$(git -C .deps/RBuild rev-parse HEAD)
+        git -C .deps/RBuild pull
+        new_head=$(git -C .deps/RBuild rev-parse HEAD)
+        if [ "$old_head" = "$new_head" ]; then
+            need_run=false # don't try to compile if git hasn't updated
+        fi
+    else
+        echo "Cloning RBuild"
+        git -C .deps clone --depth=1 https://github.com/RicardoLuis0/RBuild
+    fi
+
+    if [ $need_run = true ]; then
+        echo "Building RBuild"
+        
+        if command -v ./.RBuild &> /dev/null
+        then
+            cd .deps/RBuild
+            ../../.RBuild --file=RBuild.json
+            cp build/lin/release/bin/RBuild ../../.RBuild
+            cd ../..
+        else
+            cd .deps/RBuild
+            ./build.sh
+            cp build/lin/release/bin/RBuild ../../.RBuild
+            ../../.RBuild --file=RBuild.json
+            cp build/lin/release/bin/RBuild ../../.RBuild
+            cd ../..
+        fi
+    fi
+fi
+
+$RBuild --file=BuildInterpreter.json "$@"
